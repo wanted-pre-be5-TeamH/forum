b93178b88330c835303f59835bda7f972a52ac33
rojiwon0325 <bond9986@naver.com>
2022-10-28 10:04:58 +0900

    [회원인증/인프라] mapper, repository, module 구현

---
+++ src/api/auth/auth.module.ts
@@ -0,0 +1,43 @@
+import { JwtAuthGuard } from './infrastructure/guard/jwt.guard';
+import { ConfigService } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from './application/auth.service';
+import { LocalStrategy } from './infrastructure/strategy/local.strategy';
+import { JwtStrategy } from './infrastructure/strategy/jwt.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from '../user/infrastructure/user.entity';
+import { AuthController } from './presentation/auth.controller';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthMapper } from './infrastructure/auth.mapper';
+import { AuthRepository } from './infrastructure/auth.repository';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService<IEnv, true>) => {
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: { expiresIn: configService.get('JWT_EXPIRESIN') },
+        };
+      },
+    }),
+  ],
+  providers: [
+    AuthMapper,
+    AuthRepository,
+    AuthService,
+    LocalStrategy,
+    JwtStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
+  controllers: [AuthController],
+})
+export class AuthModule {}
---
+++ src/api/auth/infrastructure/auth.repository.ts
@@ -0,0 +1,32 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { IEntityMapper } from 'src/api/common/interface/mapper.interface';
+import { UserEntity } from 'src/api/user/infrastructure/user.entity';
+import { FindOneOptions, Repository } from 'typeorm';
+import { IAuth, IAuthId } from '../domain/auth.interface';
+import { IAuthRepository } from '../domain/repository.interface';
+import { AuthMapper } from './auth.mapper';
+
+@Injectable()
+export class AuthRepository implements IAuthRepository {
+  constructor(
+    @Inject(AuthMapper)
+    private readonly mapper: IEntityMapper<IAuth, UserEntity>,
+    @InjectRepository(UserEntity)
+    private readonly repository: Repository<UserEntity>,
+  ) {}
+
+  async findOne(
+    where: { username: string } | { id: IAuthId },
+  ): Promise<IAuth | null> {
+    const findOption: FindOneOptions<UserEntity> = { where };
+    const entity = await this.repository.findOne(findOption);
+    return entity == null ? null : this.mapper.toAggregate(entity);
+  }
+
+  async update(aggregate: IAuth): Promise<IAuth> {
+    const entity = this.mapper.toRootEntity(aggregate);
+    const newEntity = await this.repository.save(entity);
+    return this.mapper.toAggregate(newEntity);
+  }
+}
