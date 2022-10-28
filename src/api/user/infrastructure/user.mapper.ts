85efc2db8db455cf435b98f2958a9a8c35fe0267
rojiwon0325 <bond9986@naver.com>
2022-10-28 10:14:36 +0900

    [회원관리/인프라] mapper, repository 수정

--- src/api/user/infrastructure/user.mapper.ts
+++ src/api/user/infrastructure/user.mapper.ts
@@ -7,16 +7,38 @@ import { UserEntity } from './user.entity';
 @Injectable()
 export class UserEntityMapper implements IEntityMapper<IUser, UserEntity> {
   toAggregate(entity: UserEntity): IUser {
-    const { id, created_at, updated_at, username } = entity;
-    return User.get({ id, username, created_at, updated_at });
+    const {
+      id,
+      created_at,
+      updated_at,
+      username,
+      role,
+      birth_year,
+      gender,
+      phone,
+    } = entity;
+    return User.get({
+      id,
+      created_at,
+      updated_at,
+      username,
+      role,
+      birth_year,
+      gender,
+      phone,
+    });
   }
   toRootEntity(aggregate: IUser): UserEntity {
-    const { id, username } = aggregate;
+    const { id, username, role, birth_year, gender, phone } = aggregate;
     const entity = new UserEntity();
     if (id != 0) {
       entity.id = id;
     }
     entity.username = username;
+    entity.role = role;
+    entity.birth_year = birth_year;
+    entity.gender = gender;
+    entity.phone = phone;
     return entity;
   }
 }
