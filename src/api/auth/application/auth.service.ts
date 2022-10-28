31767cec5b226d350bc6b62b7ce7e656cd19bcd3
rojiwon0325 <bond9986@naver.com>
2022-10-28 10:08:14 +0900

    [회원인증/응용] service 구현

---
+++ src/api/auth/application/auth.service.ts
@@ -0,0 +1,32 @@
+import { ValidateUserDTO, FindOneDTO } from './auth.service.dto';
+import { IAuth } from '../domain/auth.interface';
+import { Inject, Injectable } from '@nestjs/common';
+import { httpExceptionProvider } from '../../common/provider/exception.provider';
+import { AuthRepository } from '../infrastructure/auth.repository';
+import { UserErrorMessage } from '../../user/infrastructure/user.entity';
+import { IAuthRepository } from '../domain/repository.interface';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @Inject(AuthRepository) private readonly authRepository: IAuthRepository,
+  ) {}
+
+  async findOne(where: FindOneDTO): Promise<IAuth> {
+    const auth = await this.authRepository.findOne(where);
+    if (auth == null) {
+      throw httpExceptionProvider('401', '존재하지 않는 사용자입니다.');
+    }
+    auth.updateAccessedAt();
+    return this.authRepository.update(auth);
+  }
+
+  async validate({ username, password }: ValidateUserDTO): Promise<IAuth> {
+    const auth = await this.findOne({ username });
+    const authentication = await auth.authenticte(password);
+    if (authentication) {
+      return auth;
+    }
+    throw httpExceptionProvider('401', UserErrorMessage.password);
+  }
+}
