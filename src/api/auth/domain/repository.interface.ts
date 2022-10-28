1c2f5c332ad4339f2e958e247e39924ce2cf143c
rojiwon0325 <bond9986@naver.com>
2022-10-28 09:39:29 +0900

    [회원인증/도메인] repository interface findOne parameter 수정

--- src/api/auth/domain/repository.interface.ts
+++ src/api/auth/domain/repository.interface.ts
@@ -1,6 +1,8 @@
-import { IAuth } from './auth.interface';
+import { IAuth, IAuthId } from './auth.interface';
 
 export interface IAuthRepository {
-  findOne: (where: { username: string }) => Promise<IAuth | null>;
-  update: (aggregate: IAuth) => Promise<IAuth | null>;
+  findOne: (
+    where: { username: string } | { id: IAuthId },
+  ) => Promise<IAuth | null>;
+  update: (aggregate: IAuth) => Promise<IAuth>;
 }
